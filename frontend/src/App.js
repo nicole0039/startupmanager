f094160e3b4931779fe9471f88fd6ddf05a828d9
nicole0039 <n12300039@qut.edu.au>
2025-08-19 01:57:14 +0800

    Updated App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,46 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Tasks from './pages/Tasks';
+import CatalystApp from './pages/CatalystApp';
 
-function App() {
+function AppContent() {
+  const location = useLocation();
+  
+  // Hide navbar for login, register, and all catalyst app routes
+  const hideNavbar = [
+    "/login", 
+    "/register", 
+    "/", 
+    "/app", 
+    "/catalystapp"
+  ].includes(location.pathname);
+  
   return (
-    <Router>
-      <Navbar />
+    <div className="App">
+      {!hideNavbar && <Navbar />}
       <Routes>
+        <Route path="/" element={<CatalystApp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/tasks" element={<Tasks />} />
+        <Route path="/app" element={<CatalystApp />} />
+        <Route path="/catalystapp" element={<CatalystApp />} />
+        {/* Catch-all route for 404 */}
+        <Route path="*" element={<CatalystApp />} />
       </Routes>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
